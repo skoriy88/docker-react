5f136a19a95ae05b78fd102b713fda6236a9c365
skoriy88 <33027529+skoriy88@users.noreply.github.com>
2020-08-21 15:29:06 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p className="App-intro">
-          CHANGED TWICE!!! 
+          Changed at feature branch!
         </p>
 
       </div>
